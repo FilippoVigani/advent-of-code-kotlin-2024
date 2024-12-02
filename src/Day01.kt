54fc34c2ec515627aba6804b70f7e6ada743e0f4
Filippo Vigani <vigani.filippo@gmail.com>
2024-12-02 23:03:02 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,31 @@
+import java.util.stream.IntStream.range
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val regex = Regex("(\\d+)\\s+(\\d+)")
+
+    val sortedList1 = mutableListOf<Int>()
+    val sortedList2 = mutableListOf<Int>()
+
+    readInput("Day01")
+        .forEach {
+            val match = regex.matchEntire(it) ?: throw Exception()
+
+            val number1 = match.groups[1]?.value?.toIntOrNull() ?: throw Exception()
+            val number2 = match.groups[2]?.value?.toIntOrNull() ?: throw Exception()
+
+            sortedList1.addSorted(number1)
+            sortedList2.addSorted(number2)
+        }
+
+    var total = 0
+    for (i in range(0, sortedList1.size)){
+        total += abs(sortedList1[i] - sortedList2[i])
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(total)
 }
+
+private fun MutableList<Int>.addSorted(element: Int) = this.add(this.binarySearch(element).toInsertionPoint(), element)
+
+private fun Int.toInsertionPoint() = abs(this + 1)
